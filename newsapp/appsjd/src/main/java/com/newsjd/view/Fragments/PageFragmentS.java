548b02c47f49dfab35fe7b3d00995abfdae3ebfa
死亡随机 <shengxdong@gmail.com>
2018-05-30 00:36:04 +0800

    1, define recycleview  (next will show this)

--- newsapp/appsjd/src/main/java/com/newsjd/view/Fragments/PageFragmentS.java
+++ newsapp/appsjd/src/main/java/com/newsjd/view/Fragments/PageFragmentS.java
@@ -1,6 +1,7 @@
 package com.newsjd.view.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,5 +17,41 @@ public  class PageFragmentS extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_page, container, false);
+    }
+
+    @Nullable
+    @Override
+    public View getView() {
+        return super.getView();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
