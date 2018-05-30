a1b8002d030045c8e77c7e601d66482cadebc8aa
死亡随机 <shengxdong@gmail.com>
2018-05-31 00:55:37 +0800

    1, cannot push ?

--- newsapp/appsjd/src/main/java/com/newsjd/FirstActivty.java
+++ newsapp/appsjd/src/main/java/com/newsjd/FirstActivty.java
@@ -44,7 +44,7 @@ public class FirstActivty extends AppCompatActivity {
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+//
             }
 
             @Override
